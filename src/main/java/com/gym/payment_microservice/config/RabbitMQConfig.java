e7c005cb3420c4de84003eda8769951e43a9672d
Br14nMat <1108638502@u.icesi.edu.co>
2025-09-15 06:23:06 -0500

    add converte rabbit config

--- src/main/java/com/gym/payment_microservice/config/RabbitMQConfig.java
+++ src/main/java/com/gym/payment_microservice/config/RabbitMQConfig.java
@@ -5,8 +5,11 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @Configuration
 public class RabbitMQConfig {
@@ -37,6 +40,18 @@ public class RabbitMQConfig {
     @Bean
     public Binding bindingPagosDLQ(Queue pagosDLQ, DirectExchange directExchange) {
         return BindingBuilder.bind(pagosDLQ).to(directExchange).with("pagos-dlq");
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
     }
 
 }
